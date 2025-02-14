37996317f2c98ce764a4c8cc38701dc92ce59137
[flaviofiqueli] <[2030918student.uma.pt]>
2025-02-14 14:29:45 +0000

    add added an account

--- Program.cs
+++ Program.cs
@@ -7,6 +7,9 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, GitHub!");
+            int n1 = 1;
+            int n2 = 2;
+            Console.WriteLine(n1 + n2);
         }
     }
 }
